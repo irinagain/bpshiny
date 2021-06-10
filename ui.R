6c1e9fa3c1915b143e1d66e3c933ba0ded28984b
dmaywald <84158365+dmaywald@users.noreply.github.com>
2021-06-09 20:51:07 -0500

    Added bp data sets and bp_scatter() functionality
    Need to add further customizability/interactivity

--- ui.R
+++ ui.R
@@ -26,8 +26,48 @@ ui <- fluidPage(
                mainPanel(DT::dataTableOutput("metric")))
     ),
     
-    tabPanel("Plots", fluid = T,
-             "hello world")
+  tabPanel("Plots", fluid = T,
+             sidebarLayout(
+               sidebarPanel = sidebarPanel(
+                 #Select Data Set from BP Ghana, BP Hypnos, or BP JHS
+                 selectInput(inputId = "dataSet", label = "Data Set", choices = c("BP Ghana" = "bp_ghana", "BP Hypnos" = "bp_hypnos", "BP JHS" = "bp_jhs")),
+                 
+                 #If BP Ghana is selected, customize bp_scatter() plot accordingly
+                 conditionalPanel(condition = "input.dataSet == 'bp_ghana'",
+                                  
+                                  #Group_var customization, c(2,6,7,..., 19) are the columns that can be applied to group_var (levels must be less than 10)
+                                  selectInput(inputId = "group_var_ghana", label = "Group Var", choices = names(bp_ghana[,c(2,6,7,8,9,10,11,13,15,17,18,19)]), ),
+                                  
+                                  #Wrap_var customization
+                                  selectInput(inputId = "wrap_vars_ghana",label = "Wrap Var", choices = names(bp_ghana[,c(1:21)]), multiple = T) #It doesn't seem to do anything
+                                  ), 
+                 
+                 #if BP JHS is selected, customize bp_scatter() plot accordingly
+                 conditionalPanel(condition = "input.dataSet == 'bp_jhs'", 
+                                  
+                                  #Group_var customization
+                                  selectInput(inputId = "group_var_jhs", label = "Group Var", choices = names(jhs_proc[,c(6,10,12,13,15,17,18,19,20,21,22,23)]))),
+                 
+                 #if BP Hypnos is selected, customize bp_scatter() plot accordingly
+                 conditionalPanel(condition = "input.dataSet == 'bp_hypnos'",
+                                  
+                                  #group_var customization
+                                  selectInput(inputId = "group_var_hypnos", label = "Group Var", choices = names(hypnos_proc[,c(4,5,6,10,15,16,17,18,19,20)]))),
+                 
+                 #Select Plot Type from Stages 2020 or AHA
+                 radioButtons(inputId = "plotType", label = "Plot Type", choices = c("AHA", "stages2020"), selected = "AHA"),
+                 
+                 #Further customization can be made if stages 2020 plot type is chosen
+                 conditionalPanel(condition = "input.plotType == 'stages2020'",
+                  checkboxInput(inputId = "inc.crisis", label = "Include Hypersensitive Crisis?", value = T), #include Crisis category
+                  checkboxInput(inputId = "inc.low", label = "Include Low Hypotension?", value = T) #include "low" category
+                 )
+                 
+                 
+                 ),
+               #draw the scatter plot
+               mainPanel = mainPanel(plotOutput(outputId = "bp.scatter"))
+             ))
   )
 )
 
