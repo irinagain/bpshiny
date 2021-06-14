05c60656ff560d86ca4415b33d0b61a35c8d4f7f
dmaywald <84158365+dmaywald@users.noreply.github.com>
2021-06-14 15:37:14 -0500

    Update server.R

--- server.R
+++ server.R
@@ -14,14 +14,14 @@
 shinyServer(function(input,output) {
   ######DATA######
   observeEvent(input$submit,{
-  #Creates textInput() based on what column names were selected 
+    #Creates textInput() based on what column names were selected 
     output$new1 <- renderUI({
       if(!'date' %in% input$bpcolnames){
         return(NULL)
       }else{
         textInput("date", "Date")
       }
-      })
+    })
     output$new2 <- renderUI({
       if(!'id' %in% input$bpcolnames){
         return(NULL)
@@ -87,7 +87,7 @@ shinyServer(function(input,output) {
     ext <- tools::file_ext(file$datapath)
     req(file)
     validate(need(ext == "csv", "Please upload a csv file"))
-
+    
     #Assigning data to variable 'bpdata1'
     bpdata1 = read.csv(file$datapath,header=T)
     
@@ -107,11 +107,11 @@ shinyServer(function(input,output) {
     #Using process_data function
     bpdata1 = process_data(data = bpdata1,sbp = sys,dbp = dias,bp_datetime = date,id = id, wake = wake, visit = visit,
                            hr=hr,pp=pp,map=map,rpp=rpp,DoW=dow)
-
+    
     bpdata1
-
+    
   })
-
+  
   
   ######MATRIC######
   #add metric based on the parameter it takes in
@@ -121,78 +121,78 @@ shinyServer(function(input,output) {
       return("none")
     }
   })
-    #specify first parameter and the default values
+  #specify first parameter and the default values
+  
+  #add description of first parameter
+  
+  output$help_text <- renderUI ({
+    parameter_type = parameter_type()
     
-    #add description of first parameter
+    if(parameter_type == "none"){
+      helpText("No parameters need to be specified.")
+    }
+  })
+  
+  #specify second parameter and its default values
+  
+  #add description of second parameter
+  
+  #specify third parameter and its default value
+  
+  #add description on third parameter
+  
+  #reactive function
+  
+  metric_table <- reactive({
+    parameter_type = parameter_type()
+    data = transform_data()
     
-    output$help_text <- renderUI ({
-      parameter_type = parameter_type()
-      
-      if(parameter_type == "none"){
-        helpText("No parameters need to be specified.")
-      }
-    })
-    
-    #specify second parameter and its default values
-    
-    #add description of second parameter
-    
-    #specify third parameter and its default value
-    
-    #add description on third parameter
-    
-    #reactive function
-
-    metric_table <- reactive({
-      parameter_type = parameter_type()
-      data = transform_data()
-      
-      if (is.null(input$parameter)) {
-        validate(
-          need(!is.null(input$parameter), "Please wait - Rendering")
+    if (is.null(input$parameter)) {
+      validate(
+        need(!is.null(input$parameter), "Please wait - Rendering")
+      )
+    } else if (grepl(',', input$parameter) & !grepl("\\(", input$parameter)) {
+      if (length(strsplit(input$parameter, split = ",")[[1]]) != 2) {
+        validate (
+          need(parameter_type %in% c("list", "none","time"), "Please wait - Rendering")
         )
-      } else if (grepl(',', input$parameter) & !grepl("\\(", input$parameter)) {
-        if (length(strsplit(input$parameter, split = ",")[[1]]) != 2) {
-          validate (
-            need(parameter_type %in% c("list", "none","time"), "Please wait - Rendering")
-          )
-        } else {
-          validate(
-            need(parameter_type %in% c("list", "lwrupr","lwrupr1","none","time"), "Please wait - Rendering")
-          )
-        }
-      } else if (grepl("\\(", input$parameter)) {
+      } else {
         validate(
-          need(parameter_type %in% c("nested", "none","time"), "Please wait - Rendering")
+          need(parameter_type %in% c("list", "lwrupr","lwrupr1","none","time"), "Please wait - Rendering")
         )
-      } else if (!grepl(',', input$parameter)) {
-        print(input$parameter)
-        validate(
-          need(parameter_type %in% c("value","value1","value_time", "none","time"), "Please wait - Rendering")
-        )
-        
       }
+    } else if (grepl("\\(", input$parameter)) {
+      validate(
+        need(parameter_type %in% c("nested", "none","time"), "Please wait - Rendering")
+      )
+    } else if (!grepl(',', input$parameter)) {
+      print(input$parameter)
+      validate(
+        need(parameter_type %in% c("value","value1","value_time", "none","time"), "Please wait - Rendering")
+      )
       
-      #loading bp library and using metric function
-      if(is.null(input$parameter) | parameter_type == "none"){
-        string = paste("bp::", input$metric, "(data)", sep = "")
+    }
+    
+    #loading bp library and using metric function
+    if(is.null(input$parameter) | parameter_type == "none"){
+      string = paste("bp::", input$metric, "(data)", sep = "")
+    }
+    if (input$filter_sleep_wake) {
+      if (parameter_type == "none") {
+        out_str = paste0("bp::calculate_sleep_wake(data, FUN = ", input$metric, ", calculate = \'", input$sleep_or_wake, "\', sleep_start = ", input$sleep_start, ", sleep_end = ", input$sleep_end, ")")
       }
-      if (input$filter_sleep_wake) {
-        if (parameter_type == "none") {
-          out_str = paste0("bp::calculate_sleep_wake(data, FUN = ", input$metric, ", calculate = \'", input$sleep_or_wake, "\', sleep_start = ", input$sleep_start, ", sleep_end = ", input$sleep_end, ")")
-        }
-        string = out_str
-      }
+      string = out_str
+    }
     eval(parse(text = string))
     
-   })
-
-
-output$metric <- DT::renderDataTable(metric_table(), extensions = "Buttons",
-                                     options = list(dom = "Btip",
-                                                    buttons = c("copy", "csv", "excel", "pdf", "print"),
-                                                    scrollX = TRUE))
-
+  })
+  
+  
+  output$metric <- DT::renderDataTable(metric_table(), extensions = "Buttons",
+                                       options = list(dom = "Btip",
+                                                      buttons = c("copy", "csv", "excel", "pdf", "print"),
+                                                      scrollX = TRUE))
+  
   ######PLOT######
   bp_ghana <- bp::bp_ghana
   bp_hypnos <- bp::bp_hypnos
@@ -233,19 +233,16 @@ output$metric <- DT::renderDataTable(metric_table(), extensions = "Buttons",
   })
   
   output$bp.scatter <- renderPlot({bp_scatter(plot.datasetInput(),
-                                  plot_type = input$plotType,
-                                  subj = NULL,
-                                  wrap_var = NULL,
-                                  group_var = {
-                                    if (input$dataSet == "bp_ghana"){input$group_var_ghana}
-                                    else if (input$dataSet == "bp_jhs"){input$group_var_jhs}
-                                    else if (input$dataSet == "bp_hypnos"){input$group_var_hypnos}
-                                    },
-                                  inc_crisis = input$inc.crisis,
-                                  inc_low = input$inc.low
-                                  )})
+                                              plot_type = input$plotType,
+                                              subj = NULL,
+                                              wrap_var = NULL,
+                                              group_var = {
+                                                if (input$dataSet == "bp_ghana"){c(NULL,input$group_var_ghana)}
+                                                else if (input$dataSet == "bp_jhs"){c(NULL,input$group_var_jhs)}
+                                                else if (input$dataSet == "bp_hypnos"){c(NULL,input$group_var_hypnos)}
+                                              },
+                                              inc_crisis = input$inc.crisis,
+                                              inc_low = input$inc.low
+  )})
   
 })
-  
-  
-  
