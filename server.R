9db1f1391c36791b48283337dc75aab73019a755
Irina Gaynanova <irinag@stat.tamu.edu>
2021-06-17 14:31:41 -0500

    Minor R code style corrections

--- server.R
+++ server.R
@@ -108,8 +108,8 @@ shinyServer(function(input,output) {
     dow = input$dow
     
     #Using process_data function
-    bpdata1 <<- process_data(data = bpdata1,sbp = sys,dbp = dias,date_time = date,id = id, wake = wake, visit = visit,
-                             hr=hr,pp=pp,map=map,rpp=rpp,DoW=dow)
+    bpdata1 <<- process_data(data = bpdata1, sbp = sys, dbp = dias, date_time = date, id = id, wake = wake, visit = visit,
+                             hr=hr, pp=pp, map=map,rpp=rpp, DoW=dow)
     
     bpdata1
     
