b23d7ecacb75e6b4c16251c8bed316f00b390952
dmaywald <84158365+dmaywald@users.noreply.github.com>
2021-06-30 17:05:47 -0500

    added uiOutput elements

--- ui.R
+++ ui.R
@@ -65,58 +65,11 @@ ui <- fluidPage(
     tabPanel("Plots", fluid = T,
              sidebarLayout(
                sidebarPanel = sidebarPanel(
-  
                  
-                 #If BP Ghana is selected, customize bp_scatter() plot accordingly
-                 conditionalPanel(condition = "input.fileselect == 'ghana_data'",
-                                  
-                                  #Subject customization
-                                  selectInput(inputId = "subj_ghana", label = "Subject", choices = c("", as.character(factor(bp_ghana$ID))), selected = NULL, multiple = T),
-                                  
-                                  #Group_var customization, c(2,6,7,..., 19) are the columns that can be applied to group_var (levels must be less than 10)
-                                  selectInput(inputId = "group_var_ghana", label = "Grouping Variable (1):", choices = c("", names(bp_ghana[,c(2,6,7,8,9,10,11,13,15,17,18,19)])), selected = NULL, multiple = T),
-                                  
-                                  #Wrap_var customization
-                                  selectInput(inputId = "wrap_vars_ghana",label = "Wrapping Variable (1):", choices = c("", names(bp_ghana[,c(2,6,7,8,9,10,11,13,15,17,18,19)])), selected = NULL, multiple = T) 
-                 ), 
-                 
-                 #if BP JHS is selected, customize bp_scatter() plot accordingly
-                 conditionalPanel(condition = "input.fileselect == 'jhsproc_data'", 
-                                  
-                                  #No Subject customization necessary
-                                  
-                                  #Group_var customization
-                                  selectInput(inputId = "group_var_jhs", label = "Grouping Variable (1):", choices =c("", names(jhs_proc[,c(6,10,12,13,15,17,18,19,20,21,22,23)])), selected = NULL, multiple = T),
-                                  
-                                  #Wrap_var customization
-                                  selectInput(inputId = "wrap_vars_jhs", label = "Wrapping Variable (1):", choices = c("", names(jhs_proc[,c(6,10,12,13,15,17,18,19,20,21,22,23)])), selected = NULL, multiple = T)
-                                  ),
-                 
-                 #if BP Hypnos is selected, customize bp_scatter() plot accordingly
-                 conditionalPanel(condition = "input.fileselect == 'hypnos_data'",
-                                  
-                                  #Subject customization
-                                  selectInput(inputId = "subj_hypnos", label = "Subject", choices = c("", as.character(factor(bp_hypnos$ID))), selected = NULL, multiple = T),
-                                  
-                                  #group_var customization
-                                  selectInput(inputId = "group_var_hypnos", label = "Grouping Variable (1):", choices = c("", as.character(names(hypnos_proc[,c(4,5,6,10,15,16,17,18,19,20)]))), selected = NULL, multiple = T),
-                                  
-                                  #wrap_var customization
-                                  selectInput(inputId = "wrap_vars_hypnos", label = "Wrapping Variable (1):", choices = c("", as.character(names(hypnos_proc[,c(4,5,6,10,15,16,17,18,19,20)]))), selected = NULL, multiple = T)
-                                  ),
-                 
-                 #if User's Upload is selected, customize bp_scatter() plot accordingly
-                 conditionalPanel(condition = "input.fileselect == 'input_data'",
-                                  
-                                  #Subject Customization
-                                  selectInput(inputId = "subj_input_data", label = "Subject", choices = c("", as.character(factor(jhs_proc$ID))), selected = NULL, multiple = T),
-                                  
-                                  #Group_var customization
-                                  selectInput(inputId = "group_var_input_data", label = "Grouping Variable (1):", choices = c("", names(jhs_proc[,1:ncol(jhs_proc)])),selected = NULL, multiple = T),
-                                  
-                                  #wrap_var customization
-                                  selectInput(inputId = "wrap_vars_input_data", label = "Wrapping Variable (1):", choices = c("", names(jhs_proc[,1:ncol(jhs_proc)])), selected = NULL, multiple = T)
-                 ),
+                uiOutput("input_data_subj"),
+                uiOutput("input_data_group_var"),
+                uiOutput("input_data_wrap_vars"),
+
                  
                  #Select Plot Type from Stages 2020 or AHA
                  radioButtons(inputId = "plotType", label = "Plot Type", choices = c("stages2020", "AHA"), selected = "stages2020"),
